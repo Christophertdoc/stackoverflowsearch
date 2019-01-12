96beeec7245e30816af384d65f1f49dddf2931d2
Christopher Dougherty <christophertdoc@gmail>
2019-01-12 14:12:19 -0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import SearchBox from './components/SearchBox';
+import DisplayResults from './components/DisplayResults'
 
-console.log("App.js ready")
-
-class App extends Component {   
-	render() { 
-		return ( 
-			<div className="App">
-				<header className="App-header">
-					<p>Edit <code>src/App.js</code> and save to reload.</p>
-				</header>
-
-			</div>
-		);
-	}
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      sfoJson : null 
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateJsonState = this.updateJsonState.bind(this);
+  }
+  updateJsonState(json){
+    //console.log(json);
+    this.setState({sfoJson : json});
+  }
+  handleSubmit(url){
+    fetch(url).then(response => response.json()).then(json => this.updateJsonState(json));
+  }
+  render() {
+    const {sfoJson} = this.state;
+    return (
+      <div className="App">
+        <SearchBox handleSubmit={this.handleSubmit}/>
+        {this.state.sfoJson && <DisplayResults handleCardClick={this.handleCardClick} items={sfoJson.items} />}
+      </div>
+    );
+  }
 }
 
 export default App;
