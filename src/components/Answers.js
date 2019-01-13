33fb0ce884d8de945e6c5af0ee3dbff48befe928
Christopher Dougherty <christophertdoc@gmail>
2019-01-13 14:15:55 -0500

    ...

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
 
+
 const Answers = (props) => {
 
     var answers = [];
     if(props.answer_count > 0) {
         props.answers.map((answer,i) => {
             const answer_body = answer.body;
-            let pr = {accepted : answer.is_accepted};
             let accepted = "";
             if (answer.is_accepted) {
                 accepted = "accepted";
             }
             answers.push(
-                <div className={`AnswerCard ${accepted}`} {...pr} key={i}>
+                <div className={`AnswerCard ${accepted}`} key={i}>
                     <p>{ReactHtmlParser(answer_body)}</p>
                 </div>
             );
-            return 1;
         }
     )}
-    
+
     return(
         <div className="Answers">
             <h3>Answers</h3>
