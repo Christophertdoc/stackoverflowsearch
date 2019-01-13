13692a9a9ea89956fb136e099ea824d709d5434c
Christopher Dougherty <christophertdoc@gmail>
2019-01-13 13:25:24 -0500

    question card refactor

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,81 +1,23 @@
 import React from 'react';
-import glamorous from 'glamorous';
-
-
-const QuestionCardDiv = glamorous.div({
-    padding : '15px',
-    width: '500px',
-    display : 'flex',
-    flexFlow : 'column wrap'
-})
-
-const CardTitlelabel = glamorous.label({
-    fontSize : '1em',
-    color : 'blue',
-    cursor: 'pointer'
-})
-
-const CardContentDiv = glamorous.div({
-	padding : '10px',
-	display : 'flex',
-	flexFlow : 'row wrap',
-	justifyContent : 'space-between'
-})
-
-const tagsSpan = glamorous.span({
-  	fontSize : '0.8em',
-})
-
-const ScoreDiv = glamorous.div({
-	width : '70px',
-	height : '70px',
-	borderRadius : '100%',
-
-	fontSize : '0.9em',
-	display : 'flex',
-	flexDirection : 'column',
-})
-
-const AnswersCountDiv = ScoreDiv;
-
-const Label = glamorous.label({
-	marginTop : '10px',
-	alignSelf : 'center',
-})
-
-const Tag = glamorous.label({
-	fontSize: '0.9em',
-	padding : '5px',
-	margin : '2px',
-	backgroundColor : '#8ff',
-	alignSelf: 'flex-end',
-})
 
 const QuestionCard = (props) => {
 	var tags = [];
 	props.tags.map((tag,i) =>
 		tags.push(
-			<Tag key={i}>{tag}</Tag>
+			<li key={i}>{tag}</li>
 		)
 	);
 
 	return (
-		<QuestionCardDiv>
-			<CardTitlelabel onClick={() => props.handleCardClick(props.answer_count)}>{props.title}</CardTitlelabel>
-			<CardContentDiv>
-				<ScoreDiv> 
-					<Label>Score</Label>
-					<Label>{props.score}</Label>
-				</ScoreDiv> 
-				<AnswersCountDiv>
-					<Label>Answers</Label>
-					<Label>{props.answer_count}</Label>
-				</AnswersCountDiv>
-				<Label>Views</Label>
-				<Label>{props.view_count}</Label>
-			</CardContentDiv>
-			<tagsSpan>{tags}</tagsSpan>
-		</QuestionCardDiv>
+		<div className="QuestionCard">
+			<h2 onClick={() => props.handleCardClick(props.answer_count)}>{props.title}</h2>
+			<h3>Score: {props.score}</h3>
+			<h3>Answers: {props.answer_count}</h3>
+			<h3>Views: {props.view_count}</h3>
+			<ul>
+				{tags}
+			</ul>
+		</div>
 	);
 }
 
