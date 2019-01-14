fee83b81fc5a17c5c39499384a44e18d9ca8d654
Christopher Dougherty <christophertdoc@gmail>
2019-01-13 19:44:51 -0500

    ..

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import Answers from './Answers'
 import ReactHtmlParser from 'react-html-parser';
 
 
 const Modal = (props) => {
+
+    const Answers = (props) => {
+        var answers = [];
+        if(props.answer_count > 0) {
+            props.answers.map((answer,i) => {
+                const answer_body = answer.body;
+                let accepted = "";
+                if (answer.is_accepted) {
+                    accepted = "accepted";
+                }
+                answers.push(
+                    <div className={`AnswerCard ${accepted}`} key={i}>
+                        <p>{ReactHtmlParser(answer_body)}</p>
+                    </div>
+                );
+            }
+        )}
+        return(
+            <div className="Answers">
+                <h3>Answers</h3>
+                {answers.length > 0 ? answers : <h3>No answers yet</h3>}
+            </div>
+        )
+    }
+
     return (
         <div className="Modal"> 
             <h1>{props.title}</h1>
