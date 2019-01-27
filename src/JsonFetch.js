0bbc3362141a82627e4edcfa17a0c77ba11ceccf
Christopher Dougherty <christophertdoc@gmail>
2019-01-27 14:55:12 -0500

    refine search

--- src/JsonFetch.js
+++ src/JsonFetch.js
@@ -3,10 +3,6 @@ export const fetchQuestionsByIdUrl = (id) => {
     return url;
 } 
 export const fetchQuestionsByTagUrl = (tag) => {
-    // let url = 'https://api.stackexchange.com/2.2/questions?pagesize=30&order=asc&sort=activity&tagged='+tag+'&site=stackoverflow&filter=!-*f(6rc.(Jba';
-    // let url = 'https://api.stackexchange.com/2.2/search/advanced?pagesize=50&order=desc&sort=activity&q='+tag+'&site=stackoverflow';
-    // let url = 'https://api.stackexchange.com/2.2/questions?pagesize=50&order=desc&sort=activity&q='+tag+'&site=stackoverflow&filter=!-*f(6rc.(Jba';
-    // let url = 'https://api.stackexchange.com/2.2/questions?pagesize=50&order=desc&sort=activity&q='+tag+'&site=stackoverflow&filter=!-*f(6rc.(Jba';
     let url = 'https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q='+tag+'&site=stackoverflow&filter=!-*f(6rc.(Jba'; 
     return (
         url
